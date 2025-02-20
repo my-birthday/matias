66ad8a0512b91be18f1eacde88a1d2fb71101353
my-birthday <micumple998@gmail.com>
2025-02-20 00:03:13 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,657 @@
+window.sr = ScrollReveal();
+
+	
+	sr.reveal('.nombre', {
+		duration: 3500, //tiempo
+		delay:1500,
+		origin: 'bottom', //origen
+		distance: '200px' //distancia
+
+	}); 
+	sr.reveal('.diaP', {
+	rotate: {
+        x: 100,
+        z: 100
+    },
+		duration: 3000, //tiempo
+		delay:2500,
+		origin: 'bottom', //origen
+		distance: '0px' //distancia
+
+
+	}); 
+sr.reveal('.nombres1', {
+	rotate: {
+        x: 100,
+        z: 100
+    },
+		duration: 4000, //tiempo
+		delay:1000,
+		origin: 'bottom', //origen
+		
+		distance: '100px' //distancia
+
+
+	}); 
+	sr.reveal('.diaF', {
+		duration: 3500, //tiempo
+		delay:1500,
+		origin: 'left', //origen
+		distance: '200px' //distancia
+
+	}); 
+		sr.reveal('.mesF', {
+		duration: 3500, //tiempo
+		delay:1500,
+		origin: 'top', //origen
+		distance: '200px' //distancia
+
+	}); 
+			sr.reveal('.banderas', {
+		duration: 4500, //tiempo
+		delay:1500,
+		origin: 'top', //origen
+		distance: '50px' //distancia
+
+	}); 
+				sr.reveal('#cuenta', {
+		duration: 3500, //tiempo
+		delay:1500,
+		origin: 'bottom', //origen
+		distance: '200px' //distancia
+
+	}); 
+sr.reveal('.music', {
+		duration: 2000, //tiempo
+		delay:100,
+		origin: 'bottom', //origen
+		
+		distance: '100px' //distancia
+
+
+	}); 
+sr.reveal('.añoP', {
+		duration: 2000, //tiempo
+		delay:2000,
+		origin: 'bottom', //origen
+		
+		distance: '50px' //distancia
+
+
+	}); 
+sr.reveal('.mesP', {
+		duration: 2000, //tiempo
+		delay:1300,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+sr.reveal('.mibautizo', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		
+		distance: '50px' //distancia
+
+
+	}); 
+sr.reveal('.music', {
+		duration: 2000, //tiempo
+		//delay:1000,
+		origin: 'top', //origen
+		
+		distance: '50px' //distancia
+
+
+	}); 
+sr.reveal('.respetuosamente', {
+		duration: 2000, //tiempo
+		delay:1500,
+		origin: 'left', //origen
+		
+		distance: '150px' //distancia
+
+
+	}); 
+sr.reveal('.diaBautizo', {
+	
+	rotate: {
+        x: 50,
+        z: 100
+    },
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'left', //origen
+		distance: '100px' //distancia
+
+
+	}); 
+
+	sr.reveal('.nuestraBoda', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'left', //origen
+		distance: '100px', //distancia
+		
+
+	});
+	
+	sr.reveal('.cuentaRegresiva', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'bottom', //origen
+		
+		distance: '100px' //distancia
+
+
+	}); 
+
+	sr.reveal('.mesaregalos', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'bottom', //origen
+		
+		distance: '100px' //distancia
+
+
+	}); 
+	sr.reveal('.boletos', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'bottom', //origen
+		
+		distance: '100px' //distancia
+
+
+	}); 
+		sr.reveal('.dondeycuandotitulo', {
+		duration: 2500, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		distance: '100px', //distancia
+		
+
+	});
+	sr.reveal('.textoNosCasamos', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		distance: '200px', //distancia
+		
+
+	});
+	sr.reveal('.dondeyCuandoMesa', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'left', //origen
+		distance: '200px', //distancia
+		
+
+	});
+		sr.reveal('.dia', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+		sr.reveal('.mes', {
+		duration: 1500, //tiempo
+		delay:100,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+		sr.reveal('.año', {
+		duration: 1500, //tiempo
+		delay:100,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+	
+	sr.reveal('.ceremoniaReligiosa', {
+		duration: 1500, //tiempo
+		///delay:1000,
+		origin: 'top', //origen
+		scale: 0.30,
+		distance: '-0' //distancia
+		
+
+	});
+		sr.reveal('.hotel4', {
+		duration: 1500, //tiempo
+		//delay:1500,
+		origin: 'left', //origen
+		distance: '150px', //distancia
+		
+
+	});
+			sr.reveal('.hotel3', {
+		duration: 1500, //tiempo
+		//delay:1500,
+		origin: 'right', //origen
+		distance: '150px', //distancia
+		
+
+	});
+	sr.reveal('.mapas', {
+		duration: 2000, //tiempo
+		//delay:100,
+		origin: 'top', //origen
+		distance: '200px', //distancia
+		
+
+	});
+	sr.reveal('.vestimenta', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+		
+
+	});
+	
+	sr.reveal('.imagen1', {
+		duration: 1000, //tiempo
+		//delay:800,
+		origin: 'top', //origen
+		distance: '300px', //distancia
+		
+
+	});
+	sr.reveal('.collage', {
+		duration: 1000, //tiempo
+		///delay:1000,
+		origin: 'top', //origen
+		scale: 0.50,
+		distance: '-0' //distancia
+		
+
+	});
+		sr.reveal('.imagen3', {
+		duration: 1000, //tiempo
+		//delay:500,
+		origin: 'bottom', //origen
+		distance: '200px', //distancia
+		
+
+	});
+
+			sr.reveal('.apellidos', {
+		duration: 1500, //tiempo
+		//delay:700,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+			sr.reveal('.f1', {
+		duration: 1500, //tiempo
+		delay:1100,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+		sr.reveal('.f2', {
+		duration: 1500, //tiempo
+		delay:1300,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+		sr.reveal('.f3', {
+		duration: 1500, //tiempo
+		delay:1500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+		sr.reveal('.f4', {
+		duration: 1500, //tiempo
+		delay:1700,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+		sr.reveal('.f5', {
+		duration: 1500, //tiempo
+		delay:100,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+
+			sr.reveal('.form-register', {
+		duration: 1000, //tiempo
+		//delay:800,
+		origin: 'top', //origen
+		distance: '300px', //distancia
+		
+
+	});
+			sr.reveal('.', {
+		duration: 2500, //tiempo
+		delay:1700,
+		origin: 'left', //origen
+		distance: '100px' //distancia
+
+
+	}); 
+					sr.reveal('.', {
+		duration: 2500, //tiempo
+		delay:1700,
+		origin: 'top', //origen
+		distance: '100px' //distancia
+
+
+	}); 
+sr.reveal('.nosCasamos', {
+		duration: 1500, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+sr.reveal('.dondeyCuando', {
+		duration: 2000, //tiempo
+		delay: 1000,
+		origin: 'left', //origen
+		distance: '150px', //distancia
+		
+
+	});
+
+sr.reveal('.porqueformas', {
+		duration: 2000, //tiempo
+		delay: 1000,
+		origin: 'bottom', //origen
+		distance: '150px', //distancia
+		
+
+	});
+sr.reveal('.pies', {
+		duration: 2000, //tiempo
+		delay: 1000,
+		origin: 'bottom', //origen
+		distance: '150px', //distancia
+		
+
+	});
+sr.reveal('.flores', {
+		duration: 3500, //tiempo
+		delay: 1300,
+		origin: 'top', //origen
+		distance: '60px', //distancia
+		
+
+	});
+sr.reveal('.ceremoniaReligiosa2', {
+		duration: 3000, //tiempo
+		delay: 1000,
+		origin: 'bottom', //origen
+		distance: '400px', //distancia
+		
+
+	});
+sr.reveal('.horas', {
+		duration: 2000, //tiempo
+		delay: 1500,
+		origin: 'top', //origen
+		distance: '100px', //distancia
+		
+
+	});
+sr.reveal('.lugar', {
+		duration: 2000, //tiempo
+		delay: 1000,
+		origin: 'left', //origen
+		distance: '150px', //distancia
+		
+
+	});
+sr.reveal('.textodondeycuando', {
+		duration: 2000, //tiempo
+		delay: 1000,
+		origin: 'left', //origen
+		distance: '150px', //distancia
+		
+
+	});
+sr.reveal('.btnComoLlegar', {
+		duration: 2000, //tiempo
+		delay: 1500,
+		origin: 'bottom', //origen
+		distance: '150px', //distancia
+		
+
+	});
+sr.reveal('.btnComoLlegar2', {
+		duration: 2000, //tiempo
+		delay: 1700,
+		origin: 'bottom', //origen
+		distance: '150px', //distancia
+		
+
+	});
+sr.reveal('.dondeyCuando2', {
+		duration: 2000, //tiempo
+		delay: 1500,
+		origin: 'top', //origen
+		distance: '100px', //distancia
+		
+
+	});
+sr.reveal('#amazonliverpool', {
+		duration: 2000, //tiempo
+		delay: 1500,
+		origin: 'bottom', //origen
+		distance: '150px', //distancia
+		
+
+	});
+sr.reveal('.foto', {
+		duration: 4000, //tiempo
+		delay: 1500,
+		origin: 'bottom', //origen
+		distance: '200px', //distancia
+		
+
+	});
+sr.reveal('.tespero', {
+		duration: 2000, //tiempo
+		delay: 1000,
+		origin: 'bottom', //origen
+		distance: '150px', //distancia
+		
+
+	});
+sr.reveal('.btnWhatsNovios', {
+		duration: 3500, //tiempo
+		delay: 1000,
+		origin: 'bottom', //origen
+		distance: '200px', //distancia
+		
+
+	});
+sr.reveal('.carousel', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+sr.reveal('.Tituloasistencia', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
+sr.reveal('.Tituloasistenciaespero', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
+
+sr.reveal('.nompadres', {
+		duration: 1500, //tiempo
+		delay:1500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+sr.reveal('.tiendas', {
+		duration: 1500, //tiempo
+		//delay:700,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+
+sr.reveal('.textoasistencia', {
+		duration: 3500, //tiempo
+		delay: 1000,
+		origin: 'top', //origen
+		distance: '200px', //distancia
+		
+
+	});
+
+sr.reveal('.textoSeccion3', {
+		duration: 1500, //tiempo
+		origin: 'left', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
+	sr.reveal('.piepagina', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	}); 
+
+
+	sr.reveal('.table-responsive', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+	sr.reveal('.linea', {
+		duration: 700, //tiempo
+		///delay:1000,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+		
+
+	});
+
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
+
+
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+ function copiar(){
+  var origen = document.getElementById('target1');
+  var destino = document.getElementById('target10');
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = origen.value;
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  alert("Copiado al portapapeles!");
+  body.removeChild(copyFrom);
+  destino.focus();
+  document.execCommand('paste');
+}
+
